8300f5b7c8fa076bab491da89652722666a7b1b5
tao.wang <331871902@qq.com>
2022-05-30 23:18:37 +0800

    feature：优化

--- js/api/user.js
+++ js/api/user.js
@@ -2,32 +2,23 @@
  * @Author: wangtao
  * @Date: 2022-04-10 17:38:30
  * @LastEditors: 汪滔
- * @LastEditTime: 2022-05-26 11:22:31
+ * @LastEditTime: 2022-05-30 23:17:33
  * @Description: file content
  */
 import axiosApi from "./AxiosApi.js";
 
-const apiList = {
-  login: {
-    method: "post",
-    url: "passport-app-api/smsLogin"
-  },
-  getConfigValue: {
-    method: "get",
-    url: "basicdata-api/system/getConfigValue"
-  }
-};
-
 export default {
   login(data) {
     return axiosApi({
-      ...apiList.login,
+      method: "post",
+      url: "passport-app-api/smsLogin",
       data
     });
   },
   getConfigValue(data) {
     return axiosApi({
-      ...apiList.getConfigValue,
+      method: "get",
+      url: "basicdata-api/system/getConfigValue",
       data
     });
   }
