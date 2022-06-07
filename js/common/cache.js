97405d4cb3a07ba0d13a3a42f5802b5abc80d2ac
tao.wang <331871902@qq.com>
2022-06-07 15:30:56 +0800

    🎸特性(js): 统一

--- js/common/cache.js
+++ js/common/cache.js
@@ -2,10 +2,11 @@
  * @Author: wangtao
  * @Date: 2020-06-28 15:43:56
  * @LastEditors: 汪滔
- * @LastEditTime: 2020-10-12 10:33:13
+ * @LastEditTime: 2022-06-07 15:29:20
  * @Description: storage的key，定义为常量统一存放，定义规则全大写（与普通变量区分，表常量）转全小写，
  * 如：USER: 'user'    APP_TOKEN: 'app_token'
  */
 export default {
-  USER: "user" // APP 登录后获取user信息
+  TOKEN_ID: "token_id", // token_id
+  USER_BASE_INFO: "user_base_info" // APP 登录后获取user信息
 };
